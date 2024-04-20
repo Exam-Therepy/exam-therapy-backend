a71d79d7161e23d90711a58f93f42e629567bb43
Inzamul Hoque <inzamol@gmail.com>
2024-04-20 03:21:14 -0700

    sign up

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MainUser, Role
+
+admin.site.register(MainUser)
+admin.site.register(Role)
 
 # Register your models here.
--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
+import json
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 
 from user.models import MainUser
@@ -9,5 +12,22 @@ from user.models import MainUser
 
 @api_view(['POST'])
 def signup(request):
-    print(request.body)
-    return JsonResponse({"Hello": "World"})
+    response = {}
+    try:
+        body = request.body
+        dictData = json.loads(body)
+        user, created = MainUser.objects.get_or_create(**dictData)
+        if not created:
+            response['status_code'] = status.HTTP_409_CONFLICT
+            response['message'] = "User already exist"
+        elif not user:
+            response['status_code'] = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response['message'] = "Something went wrong"
+        else:
+            response['status_code'] = status.HTTP_201_CREATED
+            response['message'] = "Sign up successfully"
+
+    except Exception as e:
+        response['status_code'] = status.HTTP_500_INTERNAL_SERVER_ERROR
+        response['message'] = str(e)
+    return JsonResponse(response, status=response['status_code'])
